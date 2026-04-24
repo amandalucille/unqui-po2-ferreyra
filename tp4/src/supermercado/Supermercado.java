9c45a78d68670afd7a3b59f7edad561b363c2df4
llucillell <amandac4e@gmail.com>
2026-04-24 00:43:08 -0300

    avance

--- tp4/src/supermercado/Supermercado.java
+++ tp4/src/supermercado/Supermercado.java
@@ -1,7 +1,25 @@
 package supermercado;
+import java.util.List;     
+import java.util.ArrayList;
 
 public class Supermercado {
 		private String nombre;
-		private Double direccion;
+		private String direccion;
+		private List<Producto> productos = new ArrayList<>();
 		
+	
+public Supermercado(String nombre, String direccion) { // constructor básico
+    this.nombre = nombre;
+    this.direccion = direccion;
+	
+	}
+public int getCantidadDeProductos(List<Producto> producto) {
+	return productos.size(); 
+	}
+
+
+
+public void agregarProducto(Producto producto) {
+	this.productos.add(producto);
+	}
 }
